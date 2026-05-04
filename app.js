d2e99bc734dcff88993a19db7d099c0c801dc983
emiliocardenas-crypto <emilio.cardenas@codedbykids.org>
2026-05-04 22:15:20 +0000

    THe

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 function greet (app){
-    console.log("My favoret App is " + app + " because it is fun!")
+    console.log("My favoret App is " + app + " because it is fun! I am going to the moon but their is a big problem:(")
 }
 greet("Minecraft")
 function add(num1, num2){
     return (num1 + num2);
 }
+function double (num3){
+    return num3 +num3;
+}
+const m2numbers = double (330);
+console.log(m2numbers);
+
+function half (num4){
+    return num4/2;
+}
+const dnumber2 = half (23);
+console.log(dnumber2) 
